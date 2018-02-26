f892c41780f6f0d07291420566949055d98360a4
fanzhiyong <fanzhiyong@idealsee.cn>
2018-02-26 15:22:14 +0800

    显示三角。

--- src/HelloWorld/HelloWorld.cpp
+++ src/HelloWorld/HelloWorld.cpp
@@ -8,6 +8,9 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
+bool createVertexShader(GLuint & id);
+bool createFragmentShader(GLuint & id);
+
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -45,6 +48,48 @@ int main()
 		return -1;
 	}
 
+	// 顶点着色器
+	GLuint vertexShader = 0;
+	if (!createVertexShader(vertexShader))
+	{
+		return -1;
+	}
+
+	// 片段着色器
+	GLuint fragmentShader = 0;
+	if (!createFragmentShader(fragmentShader))
+	{
+		return -1;
+	}
+
+	// link shader
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// use
+	glUseProgram(shaderProgram);
+	// clear
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// 数据
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -55,8 +100,11 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
@@ -85,4 +133,55 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	// make sure the viewport matches the new window dimensions; note that width and 
 	// height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
+}
+
+bool checkCompileStatus(unsigned int id)
+{
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(id, 512, NULL, infoLog);
+		std::cout << "Error: " << infoLog;
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool createVertexShader(GLuint & id)
+{
+	const char * vertexShaderSource =
+		"#version 330 core\n"
+		"layout(location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\n";
+
+	id = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(id, 1, &vertexShaderSource, NULL);
+	glCompileShader(id);
+
+	return checkCompileStatus(id);
+}
+
+bool createFragmentShader(GLuint & id)
+{
+	const char * fragmentShaderSource =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n";
+
+	id = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(id, 1, &fragmentShaderSource, NULL);
+	glCompileShader(id);
+
+	return checkCompileStatus(id);
 }
