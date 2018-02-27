25da5a1dbeef8680e3424a83adc025acf8f84b5e
fanzhiyong <fanzhiyong@idealsee.cn>
2018-02-27 10:35:58 +0800

    修正纹理颠倒的问题。

--- src/Exercise/myopengl.cpp
+++ src/Exercise/myopengl.cpp
@@ -99,6 +99,7 @@ void MyOpenGL::init()
     // 参数
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
     glGenerateMipmap(GL_TEXTURE_2D);
 
     m_timer->start();
